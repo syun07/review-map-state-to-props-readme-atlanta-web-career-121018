2f33541c2e58ee6e5546568de57dd01a4b13ec3e
github username <github email address>
2019-03-08 13:57:34 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { connect } from 'react-redux'
 import shoppingListItemReducer from './reducers/shoppingListItemReducer';
 import App from './App';
 import './index.css';
